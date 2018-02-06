5184138c4cd601e20843ba712edac6fb2a9ceed8
Robert <robbanl@kth.se>
2018-02-06 10:58:39 +0100

    fail test :(

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 class UnitTests(unittest.TestCase):
 
         def test_Test(self):
-            self.assertTrue(True)
+            self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
